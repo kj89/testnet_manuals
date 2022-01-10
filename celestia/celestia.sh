2d39440377cb1a2ff48735fab51248d4342ac24f
Kristaps Jahimovics <Kristaps.Jahimovics@also.com>
2022-01-11 01:27:36 +0700

    test

--- celestia/celestia.sh
+++ celestia/celestia.sh
@@ -14,20 +14,19 @@ apt-get update &&
     apt-get -o Dpkg::Options::="--force-confold" dist-upgrade -q -y --force-yes
 sleep 3
 
-sudo apt-get install build-essential -y && sudo apt-get install jq -y
-sleep 1
-
 sudo rm -rf /usr/local/go
 curl https://dl.google.com/go/go1.17.2.linux-amd64.tar.gz | sudo tar -C/usr/local -zxvf -
-cat <<'EOF' >> $HOME/.bash_profile
+cat <<'EOF' >>$HOME/.profile
 export GOROOT=/usr/local/go
 export GOPATH=$HOME/go
 export GO111MODULE=on
 export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
 EOF
-. $HOME/.bash_profile
-cp /usr/local/go/bin/go /usr/bin
+source $HOME/.profile
 go version
+
+sudo apt-get install build-essential -y && sudo apt-get install jq -y
+sleep 1
 
 cd $HOME
 git clone https://github.com/celestiaorg/celestia-app.git
@@ -72,6 +71,6 @@ EOF
 
 sudo systemctl enable celestia-appd
 sudo systemctl daemon-reload
-sudo systemctl start celestia-appd
+sudo systemctl restart celestia-appd
 
 echo 'Node status:'$(sudo service celestia-appd status | grep active)
