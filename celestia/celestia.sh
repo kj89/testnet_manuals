905678f9e562ee581a3854ca7514c77373996390
Kristaps Jahimovics <Kristaps.Jahimovics@also.com>
2022-01-11 00:51:09 +0700

    install only bare minimums dependencies

--- celestia/celestia.sh
+++ celestia/celestia.sh
@@ -10,14 +10,10 @@ echo 'Your node name: ' $CELESTIA_NODENAME
 sleep 2
 sudo dpkg --configure -a
 sudo apt update
-sudo apt install curl -y < "/dev/null"
+sudo apt upgrade -y < "/dev/null"
 sleep 3
 
-curl https://deb.nodesource.com/setup_14.x | sudo bash
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt upgrade -y < "/dev/null"
-sudo apt install nodejs=14.* yarn build-essential jq git -y < "/dev/null"
+sudo apt-get install build-essential -y && sudo apt-get install jq -y
 sleep 1
 
 sudo rm -rf /usr/local/go
