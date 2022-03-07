8c36ca20291af1ac4c097c1c55d203e89a281d4b
Kristaps Jahimovics <Kristaps.Jahimovics@also.com>
2022-03-07 22:16:57 +0200

    Make code look celaner

--- agoric/agoric_synctime.py
+++ agoric/agoric_synctime.py
@@ -21,29 +21,31 @@ def get_sync_status(url):
 
 if get_sync_status(my_rpc) == False:
     print('Your node is already synced!')
-    quit()
-
-print(f'Calculating blocks... Please wait {test_time} minutes to finish!')
-
-result = []
-for _ in range(test_time):
+else:
     current_block = get_block_height(my_rpc)
-    sleep(60)
-    result.append(get_block_height(my_rpc) - current_block)
-    avg_blocks_per_min = average(result)
-    print(f'({_ + 1}) Last bpm: {result[-1]} Average bpm: {avg_blocks_per_min}')
+    latest_block = get_block_height(public_rpc)
+    blocks_left = latest_block - current_block
+    
+    print(f'Your current block: {current_block}')
+    print(f'Latest block: {latest_block}')
+    print(f'Blocks left to sync : {blocks_left}')
 
-current_block = get_block_height(my_rpc)
-latest_block = get_block_height(public_rpc)
-blocks_left = latest_block - current_block
-min_left = int(blocks_left/avg_blocks_per_min)
-time_left = '{:02d} hours and {:02d} minutes'.format(*divmod(min_left, 60))
-print(f'{latest_block=} {current_block=}')
-print(f'Average blocks per minute: {avg_blocks_per_min}')
-print(f'Blocks left to sync : {blocks_left}')
-print(f'Time to reach latest block height: {time_left}')
+    print(f'Calculating blocks... Please wait {test_time} minutes to finish!')
 
-now = datetime.now()
-final_time = now + timedelta(minutes=min_left)
-print('Time now: ', now)
-print('Final time: ', final_time)
+    result = []
+    for _ in range(test_time):
+        current_block = get_block_height(my_rpc)
+        sleep(60)
+        result.append(get_block_height(my_rpc) - current_block)
+        avg_blocks_per_min = average(result)
+        print(f'({_ + 1}) Last bpm: {result[-1]} Average bpm: {avg_blocks_per_min}')
+
+
+    min_left = int(blocks_left/avg_blocks_per_min)
+    time_left = '{:02d} hours and {:02d} minutes'.format(*divmod(min_left, 60))
+    print(f'Average blocks per minute: {avg_blocks_per_min}')
+    print(f'Time to reach latest block height: {time_left}')
+    now = datetime.now()
+    final_time = now + timedelta(minutes=min_left)
+    print('Time now: ', now)
+    print('Final time: ', final_time)
