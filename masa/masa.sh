f6a3a45055b20acf64a64cce82e60fbc4dff988a
Kristaps Jahimovics <Kristaps.Jahimovics@also.com>
2022-03-23 15:41:21 +0200

    add masa additional boot nodes

--- masa/masa.sh
+++ masa/masa.sh
@@ -62,7 +62,7 @@ ExecStart=$(which geth) \
   --http --http.corsdomain "*" --http.vhosts "*" --http.addr 127.0.0.1 --http.port 8545 \
   --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,istanbul \
   --maxpeers 50 \
-  --bootnodes enode://91a3c3d5e76b0acf05d9abddee959f1bcbc7c91537d2629288a9edd7a3df90acaa46ffba0e0e5d49a20598e0960ac458d76eb8fa92a1d64938c0a3a3d60f8be4@54.158.188.182:21000
+  --bootnodes enode://91a3c3d5e76b0acf05d9abddee959f1bcbc7c91537d2629288a9edd7a3df90acaa46ffba0e0e5d49a20598e0960ac458d76eb8fa92a1d64938c0a3a3d60f8be4@54.158.188.182:21000,enode://571be7fe060b183037db29f8fe08e4fed6e87fbb6e7bc24bc34e562adf09e29e06067be14e8b8f0f2581966f3424325e5093daae2f6afde0b5d334c2cd104c79@142.132.135.228:21000,enode://269ecefca0b4cd09bf959c2029b2c2caf76b34289eb6717d735ce4ca49fbafa91de8182dd701171739a8eaa5d043dcae16aee212fe5fadf9ed8fa6a24a56951c@65.108.72.177:21000,enode://d2afd84a880543a63f76d03a5e7a512e3b4d811c1d2d899dd9dd250ab093d892f05c559b6822498c5e29ad029ada71a93ad17e4a77f1d83474de417b4757ace2@82.146.35.68:30300
 Restart=on-failure
 RestartSec=10
 LimitNOFILE=4096
