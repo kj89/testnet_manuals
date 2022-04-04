ec3c48c1bda90d198e1f8a222fb76c01a312cc5d
Kristaps Jahimovics <Kristaps.Jahimovics@also.com>
2022-04-04 23:41:34 +0300

    Update minimums gas fee and peers

--- archway/archway.sh
+++ archway/archway.sh
@@ -55,10 +55,16 @@ archwayd config keyring-backend file
 wget -qO $HOME/.archway/config/genesis.json "https://raw.githubusercontent.com/kj89/testnet_manuals/main/archway/genesis.json"
 wget -qO $HOME/.archway/config/addrbook.json "https://raw.githubusercontent.com/kj89/testnet_manuals/main/archway/addrbook.json"
 
+# set minimum gas price
+sed -i.bak -e "s/^minimum-gas-prices = \"\"/minimum-gas-prices = \"0august\"/" $HOME/.archway/config/app.toml
+
 # set peers and seeds
-seeds="2f234549828b18cf5e991cc884707eb65e503bb2@34.74.129.75:31076,c8890bcde31c2959a8aeda172189ec717fef0b2b@95.216.197.14:26656"
-PEERS="1f6dd298271684729d0a88402b1265e2ae8b7e7b@162.55.172.244:26656"
-sed -i.bak -e "s/^seeds *=.*/seeds = \"$seeds\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.archway/config/config.toml
+SEEDS="2f234549828b18cf5e991cc884707eb65e503bb2@34.74.129.75:31076,c8890bcde31c2959a8aeda172189ec717fef0b2b@95.216.197.14:26656"
+PEERS="332dea7332a0c4647a147a08bf50bb2038931e4c@81.30.158.46:26656,4e08eb9d62607d05e3fa3fa52d98a00014c8040b@162.55.90.254:26656,4a701d399a0cd4a577e5b30c9d3cc5d75854936e@95.214.53.132:26456,0c019ac4e4f39d95355926435e50a25ed589915f@89.163.151.226:26656,b65efc14137a426a795b5e78cf34def7e5240231@89.163.164.211:26656,33baa872768e12d4100bce5eb875b90b8739a1d4@185.214.134.154:46656,76862fd5ee017b7b46f65a7ac15da12bba12f7f1@49.12.215.72:26656"
+sed -i.bak -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.archway/config/config.toml
+
+# enable prometheus
+sed -i.bak -e "s/prometheus = false/prometheus = true/" $HOME/.archway/config/config.toml
 
 # add external (if dont use sentry), port is default
 external_address=$(wget -qO- eth0.me)
