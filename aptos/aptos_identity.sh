ddc44590a481c4e22a0af20ebc86eed964199c20
Kristaps Jahimovics <Kristaps.Jahimovics@also.com>
2022-04-09 14:17:25 +0300

    Update identity check script

--- aptos/aptos_identity.sh
+++ aptos/aptos_identity.sh
@@ -6,11 +6,6 @@ then
     PRIVATE_KEY=$(cat $HOME/aptos/identity/private-key.txt)
     echo -en "\n"
     echo "=================================================="
-    echo -e "\e[1m\e[32m1. peer-info.yaml file content \e[0m" 
-    echo -en "\n"
-    cat $HOME/aptos/identity/peer-info.yaml
-    echo -en "\n"
-    echo "=================================================="
     echo -e "\e[1m\e[32m2. Your upstream peer details. You can share your peer info with other users \e[0m" 
     echo -e ' 
 '$PUBLIC_KEY':
@@ -22,7 +17,6 @@ then
     echo -e "\e[1m\e[32m3. Your identity details \e[0m" 
     echo -en "\n"
     echo -e "\e[1m\e[32mPeer Id: \e[0m" $PEER_ID
-    echo -e "\e[1m\e[32mPublic Key: \e[0m" $PUBLIC_KEY
     echo -e "\e[1m\e[32mPrivate Key:  \e[0m" $PRIVATE_KEY
     echo -en "\n"
 else
