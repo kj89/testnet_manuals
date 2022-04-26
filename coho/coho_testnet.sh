b25c59f80c68b3763de1c2d4b5d9a6403fea1a19
Kristaps Jahimovics <Kristaps.Jahimovics@also.com>
2022-04-26 13:21:36 +0300

    Update sync status command

--- coho/coho_testnet.sh
+++ coho/coho_testnet.sh
@@ -125,4 +125,4 @@ sudo systemctl restart cohod
 
 echo '=============== SETUP FINISHED ==================='
 echo -e 'To check logs: \e[1m\e[32mjournalctl -u cohod -f -o cat \e[0m'
-echo -e 'To check sync status: \e[1m\e[32mcurl -s localhost:26657/status \e[0m'
+echo -e 'To check sync status: \e[1m\e[32mcurl -s localhost:26657/status | jq .result.sync_info \e[0m'
