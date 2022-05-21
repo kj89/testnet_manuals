e6810da90208e0668f035c46cbe29a426805c57c
Kristaps Jahimovics <Kristaps.Jahimovics@also.com>
2022-05-21 22:12:58 +0300

    Fix reset command

--- sei/sei_testnet.sh
+++ sei/sei_testnet.sh
@@ -90,7 +90,7 @@ sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every
 sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/.sei-chain/config/app.toml
 
 # reset
-seid unsafe-reset-all
+seid tendermint unsafe-reset-all
 
 echo -e "\e[1m\e[32m4. Starting service... \e[0m" && sleep 1
 # create service
