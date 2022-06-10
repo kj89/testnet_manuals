d9c1ac98a7328cb1ff5f0fd8d726fcc1cbb8065b
Kristaps Jahimovics <Kristaps.Jahimovics@also.com>
2022-06-10 07:57:29 +0300

    Update to latest binary version

--- subspace/manual_install_fullnode.md
+++ subspace/manual_install_fullnode.md
@@ -42,7 +42,7 @@ sudo apt install curl jq -y
 ```
 cd $HOME
 rm -rf subspace-*
-APP_VERSION=gemini-1b-2022-june-05
+APP_VERSION=$(curl -s https://api.github.com/repos/subspace/subspace/releases/latest | jq -r ".tag_name" | sed "s/runtime-/""/g")
 wget -O subspace-node https://github.com/subspace/subspace/releases/download/${APP_VERSION}/subspace-node-ubuntu-x86_64-${APP_VERSION}
 wget -O subspace-farmer https://github.com/subspace/subspace/releases/download/${APP_VERSION}/subspace-farmer-ubuntu-x86_64-${APP_VERSION}
 chmod +x subspace-*
--- subspace/subspace_archival.sh
+++ subspace/subspace_archival.sh
@@ -48,8 +48,7 @@ sudo apt install curl jq -y
 # update executables
 cd $HOME
 rm -rf subspace-*
-#APP_VERSION=$(curl -s https://api.github.com/repos/subspace/subspace/releases/latest | jq -r ".tag_name" | sed "s/runtime-/""/g")
-APP_VERSION=gemini-1b-2022-june-05
+APP_VERSION=$(curl -s https://api.github.com/repos/subspace/subspace/releases/latest | jq -r ".tag_name" | sed "s/runtime-/""/g")
 wget -O subspace-node https://github.com/subspace/subspace/releases/download/${APP_VERSION}/subspace-node-ubuntu-x86_64-${APP_VERSION}
 wget -O subspace-farmer https://github.com/subspace/subspace/releases/download/${APP_VERSION}/subspace-farmer-ubuntu-x86_64-${APP_VERSION}
 chmod +x subspace-*
