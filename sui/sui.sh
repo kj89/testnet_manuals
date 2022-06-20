7b7ecd8a454259c4336defced986429378d01e74
Kristaps Jahimovics <Kristaps.Jahimovics@also.com>
2022-06-20 12:10:31 +0300

    Expose node endpoints

--- sui/sui.sh
+++ sui/sui.sh
@@ -39,7 +39,9 @@ sudo mv ~/sui/target/release/sui-node /usr/local/bin/
 wget -O /var/sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
 sudo cp crates/sui-config/data/fullnode-template.yaml /var/sui/fullnode.yaml
 sudo yq e -i '.db-path="/var/sui/db"' /var/sui/fullnode.yaml \
-&& yq e -i '.genesis.genesis-file-location="/var/sui/genesis.blob"' /var/sui/fullnode.yaml
+&& yq e -i '.genesis.genesis-file-location="/var/sui/genesis.blob"' /var/sui/fullnode.yaml \
+&& yq e -i '.metrics-address="0.0.0.0:9184"' /var/sui/fullnode.yaml \
+&& yq e -i '.json-rpc-address="0.0.0.0:9000"' /var/sui/fullnode.yaml
 
 # start service
 echo "[Unit]
