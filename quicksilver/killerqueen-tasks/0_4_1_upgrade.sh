ab3b8fd1765bb1a0d9f6310dcb67ab80db3691aa
Kristaps Jahimovics <Kristaps.Jahimovics@also.com>
2022-07-01 15:57:30 +0300

    Mke script look nicer

--- quicksilver/killerqueen-tasks/0_4_1_upgrade.sh
+++ quicksilver/killerqueen-tasks/0_4_1_upgrade.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 GREEN_COLOR='\033[0;32m'
 RED_COLOR='\033[0;31m'
-WITHOU_COLOR='\033[0m'
+NO_COLOR='\033[0m'
 BLOCK=98000
 VERSION=v0.4.1
+echo -e "$GREEN_COLOR YOUR NODE WILL UPDATE TO $VERSION VERSION ON BLOCK NUMBER $BLOCK $NO_COLOR\n"
 for((;;)); do
 height=$(quicksilverd status |& jq -r ."SyncInfo"."latest_block_height")
 if ((height>=$BLOCK)); then
@@ -17,12 +18,12 @@ echo "restart the system..."
 sudo systemctl restart quicksilverd
 for (( timer=60; timer>0; timer-- ))
         do
-                printf "* second restart after sleep for ${RED_COLOR}%02d${WITHOUT_COLOR} sec\r" $timer
+                printf "* second restart after sleep for ${RED_COLOR}%02d${NO_COLOR} sec\r" $timer
                 sleep 1
         done
 height=$(quicksilverd status |& jq -r ."SyncInfo"."latest_block_height")
 if ((height>$BLOCK)); then
-echo -e "$GREEN_COLOR YOUR NODE SUCCESFULLY UPDATE TO $VERSION VERSION $WITHOU_COLOR\n"
+echo -e "$GREEN_COLOR YOUR NODE SUCCESFULLY UPDATE TO $VERSION VERSION $NO_COLOR\n"
 fi
 quicksilverd version --long | head
 break
