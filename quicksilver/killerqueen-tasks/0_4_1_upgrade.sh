db5ab931c8d047a56fe9c96dc9dd9c2b4fa3e2d0
Kristaps Jahimovics <Kristaps.Jahimovics@also.com>
2022-07-01 15:55:21 +0300

    Add variables

--- quicksilver/killerqueen-tasks/0_4_1_upgrade.sh
+++ quicksilver/killerqueen-tasks/0_4_1_upgrade.sh
@@ -2,12 +2,14 @@
 GREEN_COLOR='\033[0;32m'
 RED_COLOR='\033[0;31m'
 WITHOU_COLOR='\033[0m'
+BLOCK=98000
+VERSION=v0.4.1
 for((;;)); do
 height=$(quicksilverd status |& jq -r ."SyncInfo"."latest_block_height")
-if ((height>=98000)); then
+if ((height>=$BLOCK)); then
 cd $HOME
 rm quicksilver -rf
-git clone https://github.com/ingenuity-build/quicksilver.git --branch v0.4.1
+git clone https://github.com/ingenuity-build/quicksilver.git --branch $VERSION
 cd quicksilver
 make build
 sudo chmod +x ./build/quicksilverd && sudo mv ./build/quicksilverd /usr/local/bin/quicksilverd
@@ -19,8 +21,8 @@ for (( timer=60; timer>0; timer-- ))
                 sleep 1
         done
 height=$(quicksilverd status |& jq -r ."SyncInfo"."latest_block_height")
-if ((height>98000)); then
-echo -e "$GREEN_COLOR YOUR NODE SUCCESFULLY UPDATE TO 0.4.1 VERSION $WITHOU_COLOR\n"
+if ((height>$BLOCK)); then
+echo -e "$GREEN_COLOR YOUR NODE SUCCESFULLY UPDATE TO $VERSION VERSION $WITHOU_COLOR\n"
 fi
 quicksilverd version --long | head
 break
