940b78c3dc3857480767fa52857f5c89fd493f23
Kristaps Jahimovics <Kristaps.Jahimovics@also.com>
2022-07-01 16:32:52 +0300

    Update script

--- kujira/tools/restake.sh
+++ kujira/tools/restake.sh
@@ -5,7 +5,7 @@ for (( ;; )); do
 	echo -e "\033[0;32mWaiting 30 seconds before requesting balance\033[0m"
 	sleep 30
 	AMOUNT=$(kujirad query bank balances $KUJIRA_WALLET_ADDRESS | grep amount | awk '{split($0,a,"\""); print a[2]}')
-	AMOUNT=$AMOUNT-500
+	AMOUNT=$(($AMOUNT - 500))
 	AMOUNT_STRING=$AMOUNT"ukuji"
 	echo -e "Your total balance: \033[0;32m$AMOUNT_STRING\033[0m"
 	echo -e "${PASSWORD}\n" |  kujirad tx staking delegate $KUJIRA_VALOPER_ADDRESS $AMOUNT_STRING --from $WALLET --chain-id $KUJIRA_CHAIN_ID --fees=250ukuji --yes
