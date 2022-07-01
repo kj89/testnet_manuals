184f7c6df53f55016a496010f1d6b69384116347
Kristaps Jahimovics <Kristaps.Jahimovics@also.com>
2022-07-01 15:54:22 +0300

    Correct the block height

--- quicksilver/killerqueen-tasks/0_4_1_upgrade.sh
+++ quicksilver/killerqueen-tasks/0_4_1_upgrade.sh
@@ -4,7 +4,7 @@ RED_COLOR='\033[0;31m'
 WITHOU_COLOR='\033[0m'
 for((;;)); do
 height=$(quicksilverd status |& jq -r ."SyncInfo"."latest_block_height")
-if ((height>=95800)); then
+if ((height>=98000)); then
 cd $HOME
 rm quicksilver -rf
 git clone https://github.com/ingenuity-build/quicksilver.git --branch v0.4.1
@@ -19,7 +19,7 @@ for (( timer=60; timer>0; timer-- ))
                 sleep 1
         done
 height=$(quicksilverd status |& jq -r ."SyncInfo"."latest_block_height")
-if ((height>95800)); then
+if ((height>98000)); then
 echo -e "$GREEN_COLOR YOUR NODE SUCCESFULLY UPDATE TO 0.4.1 VERSION $WITHOU_COLOR\n"
 fi
 quicksilverd version --long | head
