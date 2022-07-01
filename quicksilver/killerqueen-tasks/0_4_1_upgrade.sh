99c08845a37edf39188c426073878f8eaf608546
Kristaps Jahimovics <Kristaps.Jahimovics@also.com>
2022-07-01 15:52:27 +0300

    Add automatic binaries update script

---
+++ quicksilver/killerqueen-tasks/0_4_1_upgrade.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+GREEN_COLOR='\033[0;32m'
+RED_COLOR='\033[0;31m'
+WITHOU_COLOR='\033[0m'
+for((;;)); do
+height=$(quicksilverd status |& jq -r ."SyncInfo"."latest_block_height")
+if ((height>=95800)); then
+cd $HOME
+rm quicksilver -rf
+git clone https://github.com/ingenuity-build/quicksilver.git --branch v0.4.1
+cd quicksilver
+make build
+sudo chmod +x ./build/quicksilverd && sudo mv ./build/quicksilverd /usr/local/bin/quicksilverd
+echo "restart the system..."
+sudo systemctl restart quicksilverd
+for (( timer=60; timer>0; timer-- ))
+        do
+                printf "* second restart after sleep for ${RED_COLOR}%02d${WITHOUT_COLOR} sec\r" $timer
+                sleep 1
+        done
+height=$(quicksilverd status |& jq -r ."SyncInfo"."latest_block_height")
+if ((height>95800)); then
+echo -e "$GREEN_COLOR YOUR NODE SUCCESFULLY UPDATE TO 0.4.1 VERSION $WITHOU_COLOR\n"
+fi
+quicksilverd version --long | head
+break
+else
+echo $height
+fi
+sleep 1
+done
--- quicksilver/killerqueen-tasks/we_are_the_champions.md
+++ quicksilver/killerqueen-tasks/we_are_the_champions.md
@@ -12,3 +12,9 @@ sudo systemctl restart quicksilverd
 ```
 
 !!! DO NOT UPGRADE BEFORE CHAIN RECHES THE BLOCK `98000`!!!
+
+## As an alternative we have prepared script that should update your binary when block height is reached
+Run this in a `screen` so it will not get stopped when session disconnected 😉
+```
+wget -O 0_4_1_upgrade.sh https://raw.githubusercontent.com/kj89/testnet_manuals/main/quicksilver/killerqueen-tasks/0_4_1_upgrade.sh && chmod +x 0_4_1_upgrade.sh && ./0_4_1_upgrade.sh
+```
