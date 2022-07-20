02e9297f7379847ef25f657b7c00c5541055faf8
Kristaps Jahimovics <50621007+kj89@users.noreply.github.com>
2022-07-20 21:59:31 +0300

    Update restake.sh

--- quicksilver/tools/restake.sh
+++ quicksilver/tools/restake.sh
@@ -4,7 +4,7 @@ for (( ;; )); do
 	quicksilverd tx distribution withdraw-rewards $QUICKSILVER_VALOPER_ADDRESS --from=$WALLET --commission --chain-id=$QUICKSILVER_CHAIN_ID --yes
 	echo -e "\033[0;32mWaiting 30 seconds before requesting balance\033[0m"
 	sleep 30
-	AMOUNT=$(quicksilverd query bank balances $QUICKSILVER_WALLET_ADDRESS -oj | jq -r '.balances[] | select(.denom=="uqck") | .amount'
+	AMOUNT=$(quicksilverd query bank balances $QUICKSILVER_WALLET_ADDRESS -oj | jq -r '.balances[] | select(.denom=="uqck") | .amount')
 	AMOUNT_STRING=$AMOUNT"uqck"
 	echo -e "Your total balance: \033[0;32m$AMOUNT_STRING\033[0m"
 	quicksilverd tx staking delegate $QUICKSILVER_VALOPER_ADDRESS $AMOUNT_STRING --from $WALLET --chain-id $QUICKSILVER_CHAIN_ID --yes
