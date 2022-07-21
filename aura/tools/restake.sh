b54a8edeecd3d97f9a6f448351011f5c33e22ddd
Kristaps Jahimovics <Kristaps.Jahimovics@also.com>
2022-07-21 14:31:44 +0300

    Update restake tools

--- aura/tools/restake.sh
+++ aura/tools/restake.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 for (( ;; )); do
 	echo -e "\033[0;32mCollecting rewards!\033[0m"
-	echo -e "${PASSWORD}\n" | aurad tx distribution withdraw-rewards $AURA_VALOPER_ADDRESS --from=$WALLET --commission --chain-id=$AURA_CHAIN_ID --fees=250ueaura --yes
+	aurad tx distribution withdraw-rewards $AURA_VALOPER_ADDRESS --from=$WALLET --commission --chain-id=$AURA_CHAIN_ID --fees=250ueaura --yes
 	echo -e "\033[0;32mWaiting 30 seconds before requesting balance\033[0m"
 	sleep 30
 	AMOUNT=$(aurad query bank balances $AURA_WALLET_ADDRESS | grep amount | awk '{split($0,a,"\""); print a[2]}')
 	AMOUNT=$(($AMOUNT - 500))
 	AMOUNT_STRING=$AMOUNT"ueaura"
 	echo -e "Your total balance: \033[0;32m$AMOUNT_STRING\033[0m"
-	echo -e "${PASSWORD}\n" |  aurad tx staking delegate $AURA_VALOPER_ADDRESS $AMOUNT_STRING --from $WALLET --chain-id $AURA_CHAIN_ID --fees=250ueaura --yes
+	 aurad tx staking delegate $AURA_VALOPER_ADDRESS $AMOUNT_STRING --from $WALLET --chain-id $AURA_CHAIN_ID --fees=250ueaura --yes
 	echo -e "\033[0;32m$AMOUNT_STRING staked! Restarting in 3600 sec!\033[0m"
 	sleep 3600
 done
--- kujira/tools/restake.sh
+++ kujira/tools/restake.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 for (( ;; )); do
 	echo -e "\033[0;32mCollecting rewards!\033[0m"
-	echo -e "${PASSWORD}\n" | kujirad tx distribution withdraw-rewards $KUJIRA_VALOPER_ADDRESS --from=$WALLET --commission --chain-id=$KUJIRA_CHAIN_ID --fees=250ukuji --yes
+	kujirad tx distribution withdraw-rewards $KUJIRA_VALOPER_ADDRESS --from=$WALLET --commission --chain-id=$KUJIRA_CHAIN_ID --fees=250ukuji --yes
 	echo -e "\033[0;32mWaiting 30 seconds before requesting balance\033[0m"
 	sleep 30
 	AMOUNT=$(kujirad query bank balances $KUJIRA_WALLET_ADDRESS | grep amount | awk '{split($0,a,"\""); print a[2]}')
 	AMOUNT=$(($AMOUNT - 500))
 	AMOUNT_STRING=$AMOUNT"ukuji"
 	echo -e "Your total balance: \033[0;32m$AMOUNT_STRING\033[0m"
-	echo -e "${PASSWORD}\n" |  kujirad tx staking delegate $KUJIRA_VALOPER_ADDRESS $AMOUNT_STRING --from $WALLET --chain-id $KUJIRA_CHAIN_ID --fees=250ukuji --yes
+	 kujirad tx staking delegate $KUJIRA_VALOPER_ADDRESS $AMOUNT_STRING --from $WALLET --chain-id $KUJIRA_CHAIN_ID --fees=250ukuji --yes
 	echo -e "\033[0;32m$AMOUNT_STRING staked! Restarting in 3600 sec!\033[0m"
 	sleep 3600
 done
