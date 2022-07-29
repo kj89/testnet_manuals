0c85122743b30d81df3ed3a603ad8824652c6072
Kristaps Jahimovics <kristaps.jahimovics@gmail.com>
2022-07-29 15:13:01 +0300

    Update paloma and peers

--- paloma/paloma.sh
+++ paloma/paloma.sh
@@ -56,7 +56,7 @@ fi
 
 echo -e "\e[1m\e[32m3. Downloading and building binaries... \e[0m" && sleep 1
 # download binary
-wget -O - https://github.com/palomachain/paloma/releases/download/v0.3.0-alpha/paloma_0.3.0-alpha_Linux_x86_64.tar.gz | \
+wget -O - https://github.com/palomachain/paloma/releases/download/v0.4.0-alpha/paloma_0.4.0-alpha_Linux_x86_64.tar.gz | \
 sudo tar -C /usr/local/bin -xvzf - palomad
 sudo chmod +x /usr/local/bin/palomad
 sudo wget -P /usr/lib https://github.com/CosmWasm/wasmvm/raw/main/api/libwasmvm.x86_64.so
@@ -75,7 +75,7 @@ wget -O ~/.paloma/config/addrbook.json https://raw.githubusercontent.com/palomac
 
 # set peers and seeds
 SEEDS=""
-PEERS="4e4159a9a34509bf100d64c9e212794d34b0afb5@159.69.217.0:10656,68e4fb7ed3c792a3cf6f8c43d984d23c513b66f4@146.19.24.34:16656,5062d64908be0c55c46fb9a53af6172034b9bad7@139.59.241.140:26656,484e0d3cc02ba868d4ad68ec44caf89dd14d1845@135.181.133.93:33656,2d81fe626fcbeb39baa7f0e5f80ce397c87b2ee1@185.144.99.227:26656,97558c8ff616932dc17aa1d6ee1beb7c20e76ee1@85.12.236.11:36656,22363c50e4fcc75084d927f150636048c66b85c2@65.21.143.79:21556,983c19423a0c9a4a444d6ccd76a73ecac523b868@78.47.128.136:26656,b9c270178981db59781c3fc8f6aaed6e32adfa01@142.132.226.57:10656,ad5905c475cd13b31284c9e29ac0e388ea4068b1@135.181.59.162:21656,7bcd621295f802c5b1d7be498ec57112b8d311a9@65.108.218.48:10656"
+PEERS"8fab1d100d1c01de299788758d31d36c321c23b5@144.202.103.140:26656"
 sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.paloma/config/config.toml
 
 # set custom ports
