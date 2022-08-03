2e2b62b04b270231084672352668992864660868
Kristaps Jahimovics <kristaps.jahimovics@gmail.com>
2022-08-03 06:51:31 +0300

    Check only claimable records

--- stride/tools/claim.sh
+++ stride/tools/claim.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-RECORD=$(strided q records list-user-redemption-record --limit 10000 --output json | jq --arg WALLET_ADDRESS "$STRIDE_WALLET_ADDRESS" '.UserRedemptionRecord | map(select(.sender == $WALLET_ADDRESS))')
+RECORD=$(strided q records list-user-redemption-record --limit 10000 --output json | jq --arg WALLET_ADDRESS "$STRIDE_WALLET_ADDRESS" '.UserRedemptionRecord | map(select(.sender == $WALLET_ADDRESS and .isClaimable == true))')
 RECORD_COUNT=$(echo $RECORD | jq length)
 echo -e "\e[1m\e[32m$RECORD_COUNT\e[0m claimable records found for sender \e[1m\e[32m$STRIDE_WALLET_ADDRESS\e[0m..."
 sleep 3
@@ -7,13 +7,10 @@ for row in $(echo "${RECORD}" | jq -r '.[] | @base64'); do
     _jq() {
      echo ${row} | base64 --decode | jq -r ${1}
     }
-   if [ $(_jq '.isClaimable') = true ]
-   then
-     ZONE=$(echo $(_jq '.hostZoneId'))
-     EPOCH=$(echo $(_jq '.epochNumber'))
-     SENDER=$(echo $(_jq '.sender'))
-     echo -e "Claiming \e[1m\e[32m$ZONE.$EPOCH.$SENDER\e[0m..."
-     strided tx stakeibc claim-undelegated-tokens $ZONE $EPOCH $SENDER --chain-id $STRIDE_CHAIN_ID --from $WALLET --yes
-     sleep 10
-   fi
+    ZONE=$(echo $(_jq '.hostZoneId'))
+    EPOCH=$(echo $(_jq '.epochNumber'))
+    SENDER=$(echo $(_jq '.sender'))
+    echo -e "Claiming \e[1m\e[32m$ZONE.$EPOCH.$SENDER\e[0m..."
+    strided tx stakeibc claim-undelegated-tokens $ZONE $EPOCH $SENDER --chain-id $STRIDE_CHAIN_ID --from $WALLET --yes
+    sleep 10
 done
