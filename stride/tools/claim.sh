9fdb35e82843e67e8a1797a50731711d6a4838db
Kristaps Jahimovics <kristaps.jahimovics@gmail.com>
2022-08-03 08:17:10 +0300

    Update readme

--- stride/tools/claim.sh
+++ stride/tools/claim.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-RECORD=$(strided q records list-user-redemption-record --limit 10000 --output json | jq --arg WALLET_ADDRESS "$STRIDE_WALLET_ADDRESS" '.UserRedemptionRecord | map(select(.sender == $WALLET_ADDRESS and .isClaimable == true))')
-RECORD_COUNT=$(echo $RECORD | jq length)
-echo -e "\e[1m\e[32m$RECORD_COUNT\e[0m claimable records found for sender \e[1m\e[32m$STRIDE_WALLET_ADDRESS\e[0m..."
+RECORDS=$(strided q records list-user-redemption-record --limit 10000 --output json | jq --arg WALLET_ADDRESS "$STRIDE_WALLET_ADDRESS" '.UserRedemptionRecord | map(select(.sender == $WALLET_ADDRESS and .isClaimable == true))')
+RECORDS_COUNT=$(echo $RECORDS | jq length)
+echo -e "\e[1m\e[32m$RECORDS_COUNT\e[0m claimable records found for sender \e[1m\e[32m$STRIDE_WALLET_ADDRESS\e[0m..."
 sleep 3
-for row in $(echo "${RECORD}" | jq -r '.[] | @base64'); do
+for row in $(echo "${RECORDS}" | jq -r '.[] | @base64'); do
     _jq() {
      echo ${row} | base64 --decode | jq -r ${1}
     }
