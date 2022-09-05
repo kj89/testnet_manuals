687566dcbbb748a2de325caccc2fe33a9a07c214
Kristaps Jahimovics <kristaps.jahimovics@gmail.com>
2022-09-05 08:47:32 +0300

    Add rebus upgrade

---
+++ rebus/upgrade/995737/upgrade.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+GREEN_COLOR='\033[0;32m'
+RED_COLOR='\033[0;31m'
+NO_COLOR='\033[0m'
+BLOCK=995737
+VERSION=v0.0.4
+echo -e "$GREEN_COLOR YOUR NODE WILL BE UPDATED TO VERSION: $VERSION ON BLOCK NUMBER: $BLOCK $NO_COLOR\n"
+for((;;)); do
+	height=$(rebusd status |& jq -r ."SyncInfo"."latest_block_height")
+	if ((height>=$BLOCK)); then
+
+		sudo systemctl stop rebusd
+		cd $HOME && rm -rf rebus.core
+		git clone https://github.com/rebuschain/rebus.core.git && cd rebus.core
+		git checkout v0.0.4
+		make install
+		echo "restart the system..."
+		sudo systemctl restart rebusd && journalctl -fu rebusd -o cat
+
+		for (( timer=60; timer>0; timer-- )); do
+			printf "* second restart after sleep for ${RED_COLOR}%02d${NO_COLOR} sec\r" $timer
+			sleep 1
+		done
+		height=$(rebusd status |& jq -r ."SyncInfo"."latest_block_height")
+		if ((height>$BLOCK)); then
+			echo -e "$GREEN_COLOR YOUR NODE WAS SUCCESFULLY UPDATED TO VERSION: $VERSION $NO_COLOR\n"
+		fi
+		rebusd version --long | head
+		break
+	else
+		echo -e "${GREEN_COLOR}$height${NO_COLOR} ($(( BLOCK - height  )) blocks left)"
+	fi
+	sleep 5
+done
