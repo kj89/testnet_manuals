a70f709f6a5b66cd0bb391292f73c46aa5b52d02
Kristaps Jahimovics <kristaps.jahimovics@gmail.com>
2022-09-07 17:27:34 +0300

    Add custom ports to agoric testnet

--- agoric/emerynet/agoric.sh
+++ agoric/emerynet/agoric.sh
@@ -17,13 +17,15 @@ sleep 2
 curl https://emerynet.agoric.net/network-config > $HOME/chain.json
 
 # set vars
+AGORIC_PORT=27
 if [ ! $NODENAME ]; then
-	read -p "Enter node name: " NODENAME
-	echo 'export NODENAME='$NODENAME >> $HOME/.bash_profile
+echo "export NODENAME=$NODENAME" >> $HOME/.bash_profile
 fi
 if [ ! $WALLET ]; then
 	echo "export WALLET=wallet" >> $HOME/.bash_profile
 fi
+echo "export AGORIC_CHAIN_ID=$(jq -r .chainName < $HOME/chain.json)" >> $HOME/.bash_profile
+echo "export AGORIC_PORT=${AGORIC_PORT}" >> $HOME/.bash_profile
 source $HOME/.bash_profile
 
 echo -e "\e[1m\e[32m1. Updating packages... \e[0m" && sleep 1
@@ -56,24 +58,22 @@ if ! [ -x "$(command -v go)" ]; then
   source ~/.bash_profile
 fi
 
-# get chain id
-echo "export CHAIN_ID=$(jq -r .chainName < $HOME/chain.json)" >> $HOME/.bash_profile
-
 echo -e "\e[1m\e[32m3. Downloading and building binaries... \e[0m" && sleep 1
 # download binary
 git clone https://github.com/Agoric/ag0
 cd ag0
-git checkout agoric-upgrade-6
+git checkout agoric-upgrade-7
 make build
 . $HOME/.bash_profile
 sudo cp $HOME/ag0/build/ag0 /usr/local/bin
 
 # config
-ag0 config chain-id $CHAIN_ID
-ag0 config keyring-backend test
+ag0 config chain-id $AGORIC_CHAIN_ID
+ag0 config keyring-backend file
+ag0 config node tcp://localhost:${AGORIC_PORT}657
 
 # init
-ag0 init $NODENAME --chain-id $CHAIN_ID
+ag0 init $NODENAME --chain-id $AGORIC_CHAIN_ID
 
 # download genesis
 curl https://emerynet.rpc.agoric.net/genesis | jq .result.genesis > $HOME/.agoric/config/genesis.json 
@@ -83,44 +83,38 @@ PEERS=$(jq '.peers | join(",")' < $HOME/chain.json)
 SEEDS=$(jq '.seeds | join(",")' < $HOME/chain.json)
 sed -i -e "s/^seeds *=.*/seeds = $SEEDS/; s/^persistent_peers *=.*/persistent_peers = $PEERS/" $HOME/.agoric/config/config.toml
 
-# Fix `Error: failed to parse log level`
-sed -i.bak 's/^log_level/# log_level/' $HOME/.agoric/config/config.toml
+# set custom ports
+sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:${AGORIC_PORT}658\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:${AGORIC_PORT}657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:${AGORIC_PORT}060\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:${AGORIC_PORT}656\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":${AGORIC_PORT}660\"%" $HOME/.agoric/config/config.toml
+sed -i.bak -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:${AGORIC_PORT}317\"%; s%^address = \":8080\"%address = \":${AGORIC_PORT}080\"%; s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:${AGORIC_PORT}090\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:${AGORIC_PORT}091\"%" $HOME/.agoric/config/app.toml
 
-# enable prometheus
-sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.agoric/config/config.toml
-
-# expose rpc
-sed -i 's#"tcp://127.0.0.1:26657"#"tcp://0.0.0.0:26657"#g' $HOME/.agoric/config/config.toml
-
-# enable pruning
+# config pruning
 pruning="custom"
 pruning_keep_recent="100"
 pruning_keep_every="0"
-pruning_interval="10"
+pruning_interval="50"
 sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/.agoric/config/app.toml
 sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/.agoric/config/app.toml
 sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/.agoric/config/app.toml
 sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/.agoric/config/app.toml
 
+# enable prometheus
+sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.agoric/config/config.toml
+
 # set minimum gas price
-sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0ubld\"/" $HOME/.agoric/config/app.toml
+sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.025ubld\"/" $HOME/.agoric/config/app.toml
 
 # reset
-ag0 unsafe-reset-all
+ag0 tendermint unsafe-reset-all --home $HOME/.agoric
 
 # create service
 sudo tee /etc/systemd/system/agoricd.service > /dev/null <<EOF
 [Unit]
-Description=Agoric Cosmos daemon
+Description=agoric
 After=network-online.target
 
 [Service]
-# OPTIONAL: turn on JS debugging information.
-#SLOGFILE=.agoric/data/chain.slog
 User=$USER
-# OPTIONAL: turn on Cosmos nondeterminism debugging information
-#ExecStart=$(which ag0) start --log_level=info --trace-store=.agoric/data/kvstore.trace
-ExecStart=$(which ag0) start --log_level=info
+ExecStart=$(which ag0) start --home $HOME/.agoric
 Restart=on-failure
 RestartSec=3
 LimitNOFILE=65535
@@ -137,4 +131,4 @@ sudo systemctl restart agoricd
 
 echo '=============== SETUP FINISHED ==================='
 echo -e 'To check logs: \e[1m\e[32mjournalctl -u agoricd -f -o cat\e[0m'
-echo -e 'To check sync status: \e[1m\e[32mcurl -s localhost:26657/status | jq .result.sync_info\e[0m'
+echo -e "To check sync status: \e[1m\e[32mcurl -s localhost:${AGORIC_PORT}657/status | jq .result.sync_info\e[0m"
