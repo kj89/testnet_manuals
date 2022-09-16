4a9047b887cfd0a801f0991a4fd74c9ad441c489
Kristaps Jahimovics <kristaps.jahimovics@gmail.com>
2022-09-16 21:04:31 +0300

    Add upgrade instructions

---
+++ sei/atlantic-sub-1/upgrade/2344300/upgrade.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+GREEN_COLOR='\033[0;32m'
+RED_COLOR='\033[0;31m'
+NO_COLOR='\033[0m'
+BLOCK=2344300
+VERSION=1.1.0beta
+echo -e "$GREEN_COLOR YOUR NODE WILL BE UPDATED TO VERSION: $VERSION ON BLOCK NUMBER: $BLOCK $NO_COLOR\n"
+for((;;)); do
+	height=$(seid status |& jq -r ."SyncInfo"."latest_block_height")
+	if ((height>=$BLOCK)); then
+
+		cd $HOME && rm $HOME/sei-chain -rf
+		git clone https://github.com/sei-protocol/sei-chain.git && cd $HOME/sei-chain
+		git checkout 1.2.0beta
+		make install
+		sudo systemctl restart seid
+
+		for (( timer=60; timer>0; timer-- )); do
+			printf "* second restart after sleep for ${RED_COLOR}%02d${NO_COLOR} sec\r" $timer
+			sleep 1
+		done
+		height=$(seid status |& jq -r ."SyncInfo"."latest_block_height")
+		if ((height>$BLOCK)); then
+			echo -e "$GREEN_COLOR YOUR NODE WAS SUCCESFULLY UPDATED TO VERSION: $VERSION $NO_COLOR\n"
+		fi
+		seid version --long | head
+		break
+	else
+		echo -e "${GREEN_COLOR}$height${NO_COLOR} ($(( BLOCK - height  )) blocks left)"
+	fi
+	sleep 5
+done
