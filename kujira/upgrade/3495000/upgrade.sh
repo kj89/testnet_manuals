916d72d61daa90639e56f7d6eb5bd6c18dbfd0dc
Kristaps Jahimovics <kristaps.jahimovics@gmail.com>
2022-09-28 14:29:27 +0300

    Kujira add new upgrade to v0.6.0

---
+++ kujira/upgrade/3495000/upgrade.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+GREEN_COLOR='\033[0;32m'
+RED_COLOR='\033[0;31m'
+NO_COLOR='\033[0m'
+BLOCK=3495000
+VERSION=v0.6.0
+echo -e "$GREEN_COLOR YOUR NODE WILL BE UPDATED TO VERSION: $VERSION ON BLOCK NUMBER: $BLOCK $NO_COLOR\n"
+for((;;)); do
+	height=$(kujirad status |& jq -r ."SyncInfo"."latest_block_height")
+	if ((height>=$BLOCK)); then
+
+		sudo systemctl stop kujirad
+		cd $HOME && rm -rf core
+		git clone https://github.com/Team-Kujira/core.git --branch v0.6.0
+		cd core
+		make install
+		sudo systemctl restart kujirad && journalctl -fu kujirad -o cat
+
+		for (( timer=60; timer>0; timer-- )); do
+			printf "* second restart after sleep for ${RED_COLOR}%02d${NO_COLOR} sec\r" $timer
+			sleep 1
+		done
+		height=$(kujirad status |& jq -r ."SyncInfo"."latest_block_height")
+		if ((height>$BLOCK)); then
+			echo -e "$GREEN_COLOR YOUR NODE WAS SUCCESFULLY UPDATED TO VERSION: $VERSION $NO_COLOR\n"
+		fi
+		kujirad version --long | head
+		break
+	else
+		echo -e "${GREEN_COLOR}$height${NO_COLOR} ($(( BLOCK - height  )) blocks left)"
+	fi
+	sleep 5
+done
