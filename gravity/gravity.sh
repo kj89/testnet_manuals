d0359b7c418b4acee02aef86b483768b4d19f4c8
Kristaps Jahimovics <kristaps.jahimovics@gmail.com>
2022-10-16 14:08:24 +0300

    Update version to fix IBC vulnarability

--- gravity/gravity.sh
+++ gravity/gravity.sh
@@ -58,8 +58,8 @@ echo -e "\e[1m\e[32m3. Downloading and building binaries... \e[0m" && sleep 1
 # download binary
 cd $HOME
 mkdir gravity-bin && cd gravity-bin
-wget -O gravityd https://github.com/Gravity-Bridge/Gravity-Bridge/releases/download/v1.6.7/gravity-linux-amd64
-wget -O gbt https://github.com/Gravity-Bridge/Gravity-Bridge/releases/download/v1.6.7/gbt
+wget -O gravityd https://github.com/Gravity-Bridge/Gravity-Bridge/releases/download/v1.7.2/gravity-linux-amd64
+wget -O gbt https://github.com/Gravity-Bridge/Gravity-Bridge/releases/download/v1.7.2/gbt
 chmod +x *
 sudo mv * /usr/bin/
 
