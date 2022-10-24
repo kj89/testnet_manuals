5a36d839999c8923dfc511de8160207c302d0167
Kristaps Jahimovics <kristaps.jahimovics@gmail.com>
2022-10-24 15:16:28 +0300

    Add teritori testnet upgrade

---
+++ teritori/upgrade/testnet/1570000/upgrade.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+GREEN_COLOR='\033[0;32m'
+RED_COLOR='\033[0;31m'
+NO_COLOR='\033[0m'
+BLOCK=1570000
+VERSION=v1.3.0
+echo -e "$GREEN_COLOR YOUR NODE WILL BE UPDATED TO VERSION: $VERSION ON BLOCK NUMBER: $BLOCK $NO_COLOR\n"
+for((;;)); do
+	height=$(teritorid status |& jq -r ."SyncInfo"."latest_block_height")
+	if ((height>=$BLOCK)); then
+
+		sudo systemctl stop teritorid
+		cd $HOME && rm -rf teritori-chain
+		git clone https://github.com/TERITORI/teritori-chain.git
+		cd teritori-chain
+		git checkout $VERSION
+		make install
+		sudo systemctl restart teritorid && journalctl -fu teritorid -o cat
+
+		for (( timer=60; timer>0; timer-- )); do
+			printf "* second restart after sleep for ${RED_COLOR}%02d${NO_COLOR} sec\r" $timer
+			sleep 1
+		done
+		height=$(teritorid status |& jq -r ."SyncInfo"."latest_block_height")
+		if ((height>$BLOCK)); then
+			echo -e "$GREEN_COLOR YOUR NODE WAS SUCCESFULLY UPDATED TO VERSION: $VERSION $NO_COLOR\n"
+		fi
+		teritorid version --long | head
+		break
+	else
+		echo -e "${GREEN_COLOR}$height${NO_COLOR} ($(( BLOCK - height  )) blocks left)"
+	fi
+	sleep 5
+done
