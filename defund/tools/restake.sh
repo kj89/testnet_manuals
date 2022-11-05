6d5271963d601a6b175c42bc6ade84debcf0630a
Kristaps Jahimovics <kristaps.jahimovics@gmail.com>
2022-11-05 19:11:54 +0300

    Update restake for defund

--- defund/tools/restake.sh
+++ defund/tools/restake.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 for (( ;; )); do
 	echo -e "\033[0;32mCollecting rewards!\033[0m"
-	defundd tx distribution withdraw-rewards $DEFUND_VALOPER_ADDRESS --from=$WALLET --commission --chain-id=$DEFUND_CHAIN_ID --fees=250ufetf --yes
+	defundd tx distribution withdraw-rewards $DEFUND_VALOPER_ADDRESS --from=$WALLET --commission --chain-id=$DEFUND_CHAIN_ID --yes
 	echo -e "\033[0;32mWaiting 30 seconds before requesting balance\033[0m"
 	sleep 30
 	AMOUNT=$(defundd query bank balances $DEFUND_WALLET_ADDRESS | grep amount | awk '{split($0,a,"\""); print a[2]}')
-	AMOUNT=$(($AMOUNT - 500))
 	AMOUNT_STRING=$AMOUNT"ufetf"
 	echo -e "Your total balance: \033[0;32m$AMOUNT_STRING\033[0m"
-	 defundd tx staking delegate $DEFUND_VALOPER_ADDRESS $AMOUNT_STRING --from $WALLET --chain-id $DEFUND_CHAIN_ID --fees=250ufetf --yes
+	defundd tx staking delegate $DEFUND_VALOPER_ADDRESS $AMOUNT_STRING --from $WALLET --chain-id $DEFUND_CHAIN_ID --yes
 	echo -e "\033[0;32m$AMOUNT_STRING staked! Restarting in 3600 sec!\033[0m"
 	sleep 3600
 done
