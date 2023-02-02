749e64e245daf6a408be4255a87a4fec244032de
Kristaps Jahimovics <kristaps.jahimovics@gmail.com>
2023-02-02 12:11:19 +0200

    Update

--- nym/update.sh
+++ nym/update.sh
@@ -1,5 +1,5 @@
 node_id=$(systemctl cat nym-mixnode | grep id | awk '{print $4}' | sed 's/.$//' | sed 's/^.//')
-version=$(nym-mixnode --version | grep "version" | awk '{print $4}' | sed 's/.$//')
+version=$(nym-mixnode --version | grep "Build Version" | awk '{print $3}')
 sleep 1
 echo "You are running mixnode version" $version "with id" $node_id
 sleep 1
@@ -8,7 +8,7 @@ sudo curl https://sh.rustup.rs -sSf | sh -s -- -y
 source $HOME/.cargo/env
 echo "Downloading new binaries (v1.1.6)"
 sudo wget -O $(which nym-mixnode) https://github.com/nymtech/nym/releases/download/nym-binaries-v1.1.6/nym-mixnode
-version=$(nym-mixnode --version | grep "Build Version" | awk '{print $4}' | sed 's/.$//')
+version=$(nym-mixnode --version | grep "Build Version" | awk '{print $3}')
 echo "Current mixnode version:" $version
 echo "Initialize your mixmode"
 echo "Upgrading your mixnode config"
